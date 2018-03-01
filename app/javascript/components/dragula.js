b813516a8c29467b328023b596e5b48da6d57e10
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-03-01 17:27:31 +0100

    dragula

---
+++ app/javascript/components/dragula.js
@@ -0,0 +1,9 @@
+import dragula from 'dragula';
+
+dragula([
+  document.querySelector('#left'),
+  document.querySelector('#right'),
+  document.querySelector('#top')
+  ]);
+
+import 'dragula/dist/dragula.css';
