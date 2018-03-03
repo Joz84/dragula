0524b042054bb3c1a1ca6d0c85694ee405766992
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-03-03 01:43:32 +0100

    switch with dorp zone done

--- app/javascript/components/dragula.js
+++ app/javascript/components/dragula.js
@@ -1,10 +1,27 @@
 import dragula from 'dragula';
 
-dragula([
+const dragDecorator = (containers, options, transitZone) => {
+  const dragulaConst = dragula( containers, options);
+  dragulaConst.on('drop', (el, target, source, sibling) => {
+    if (transitZone !== target && target.children.length > 1) {
+      const element = sibling ? sibling : el;
+      element.classList.remove("gu-transit")
+      source.insertAdjacentHTML("beforeend", element.outerHTML);
+      element.remove();
+    }
+  });
+};
+
+const top = document.querySelector('#top');
+const containers = [
+  top,
   document.querySelector('#left'),
-  document.querySelector('#right'),
-  document.querySelector('#top')
-  ],
-  {revertOnSpill: true});
+  document.querySelector('#center-left'),
+  document.querySelector('#center-right'),
+  document.querySelector('#right')
+];
+const options = { };
+dragDecorator(containers, options, top)
 
 import 'dragula/dist/dragula.css';
+
